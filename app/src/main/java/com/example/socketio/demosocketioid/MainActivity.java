0427bda22ea8f444c51ce2166320795af996dfdf
varun <varunagarwal315@gmail.com>
2016-10-15 22:04:23 +0530

    id() is now working. Added comments

--- app/src/main/java/com/example/socketio/demosocketioid/MainActivity.java
+++ app/src/main/java/com/example/socketio/demosocketioid/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.socketio.demosocketioid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.socket.client.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,7 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SocketIOManager ioManager = SocketIOManager.getInstance();
-        ioManager.start();
+//        SocketIOManager ioManager = SocketIOManager.getInstance();
+//        ioManager.start();
+
+        Socket socket = new SocketIOManager().getSocketInstance();
+
+        /**
+         * Either you can place socket.connect() and the corresponding methods here or leave
+         * it in the SocketIOManager class. Up to you. For complete persistance, use a service
+         */
     }
 }
